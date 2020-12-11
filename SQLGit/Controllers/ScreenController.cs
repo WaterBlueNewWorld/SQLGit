70f73031d2aa7239b63fb2b522254910f54dada7
Angelpiza <66336392+Angelpiza@users.noreply.github.com>
2020-12-11 15:49:51 -0600

    new

---
+++ SQLGit/Controllers/ScreenController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+using DevExpress.Web;
+namespace SQLGit.Controllers
+{
+    public class ScreenController : Controller
+    {
+        // GET: Screen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
