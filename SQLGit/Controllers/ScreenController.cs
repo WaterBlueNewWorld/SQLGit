f560eb689d8c5ba98d991e9e90cce333cf2d6508
Angelpiza <66336392+Angelpiza@users.noreply.github.com>
2020-12-15 16:58:53 -0600

    EL perro index y el modal

--- SQLGit/Controllers/ScreenController.cs
+++ SQLGit/Controllers/ScreenController.cs
@@ -7,14 +7,12 @@ namespace SQLGit.Controllers
 {
     public class ScreenController : Controller
     {
-        private Var model;
+       
         // GET: Screen
         public ActionResult Index()
         {
-            
             return View();
         }
-
         public ActionResult TablaDistinta()
         {
             return PartialView("_TablaDistinta");
@@ -26,6 +24,22 @@ namespace SQLGit.Controllers
         public ActionResult EsquemaDestino()
         {
             return PartialView("_EsquemaDestino");
+        }
+        public ActionResult ServidorOrigenBox()
+        {
+            return PartialView("ServidorOrigenBox");
+        }
+        public ActionResult ServidorDestinoBox()
+        {
+            return PartialView("ServidorDestinoBox");
+        }
+        public ActionResult BaseOrigenBox()
+        {
+            return PartialView("BaseOrigenBox");
+        }
+        public ActionResult BaseDestinoBox()
+        {
+            return PartialView("BaseDestinoBox");
         }
     }
 }
